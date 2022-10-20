739bbe799eb433432a07979cbbcd24e661db1a02
Sekoutouree <edwinsamm@gmail.com>
2022-10-20 22:01:32 +0000

    Added funciton to remove and add hidden and overlay function

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
+
 const overlay = document.querySelector('.overlay');
+
 const btnCloseModal = document.querySelector('.close-modal');
+
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener('click', showModal);
+btnCloseModal.addEventListener('click', closeModal);
